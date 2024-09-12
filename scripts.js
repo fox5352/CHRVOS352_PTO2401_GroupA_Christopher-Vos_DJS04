d9f0f3c31e93a548a6931a3f1216bf0b1b9dbeba
Christopher <voschristopher53@gmail.com>
2024-09-12 11:00:04 +0200

    started using book-preview componenet in populate function

--- scripts.js
+++ scripts.js
@@ -31,24 +31,9 @@ function populateCardWindow(data) {
     const starting = document.createDocumentFragment()
 
     for (const { author, id, image, title } of data) {
-        const element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
-    
-        element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `
-    
-        starting.appendChild(element)
+        starting.appendChild(createBookPreView(author, id, image, title))
     }
+
 
     document.querySelector('[data-list-items]').appendChild(starting)
 }
